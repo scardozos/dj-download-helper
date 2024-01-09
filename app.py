6d038f670bee5b816e96b460a7cb720d8f026a98
Santi <santi.cardozosartori@gmail.com>
2024-01-09 01:16:40 +0100

    add music list refresh functionality

--- app.py
+++ app.py
@@ -10,6 +10,9 @@ supported_audio_extensions = [
     ".wav"
 ]
 
+filelist = os.listdir(DOWNLOADS_PATH)
+existing_musiclist = [filename for filename in filelist if os.path.splitext(filename)[1] in supported_audio_extensions]
+
 class Model:
     def __init__(self):
         pass
@@ -23,23 +26,37 @@ class MainPage(tk.Frame):
         tk.Frame.__init__(self, parent,width=200)
         label = tk.Label(self, text="Main Page")
         label.pack(pady=10,padx=10)
-        self.list_music_files()
 
-    def list_music_files(self):
+        self.file_list_frame = tk.Frame(master=self.parent, width=100)
+        self.file_list_frame.pack(fill=tk.BOTH, side=tk.LEFT)
+
+        self.listbox = tk.Listbox(master=self.file_list_frame, width=100, height=40)
+        self.listbox.pack(fill=tk.BOTH, side=tk.LEFT, padx=10,pady=10)
+
+        self.update_file_list()
+        self.right_side()
+
+    def right_side(self):
+        self.right_side_frame = tk.Frame(master=self.parent, width=800)
+        self.right_side_frame.pack(fill=tk.BOTH, side=tk.LEFT)
+
+    def update_file_list(self):
     
-        self.frame = tk.Frame(master=self.parent, width=200)
-        self.frame.pack(fill=tk.BOTH, side=tk.LEFT)
+        #self.listbox.delete(0, tk.END)
 
         self.filelist = os.listdir(DOWNLOADS_PATH)
-        self.musiclist = [filename for filename in self.filelist if os.path.splitext(filename)[1] in supported_audio_extensions]
-        print(self.musiclist)
+        
+        self.current_musiclist = [filename for filename in self.filelist if os.path.splitext(filename)[1] in supported_audio_extensions]
+        
+        self.musiclist = [filename for filename in self.current_musiclist if filename not in existing_musiclist]
+        # print(self.musiclist)
 
-        self.listbox = tk.Listbox(master=self.frame, width=100, height=40)
         for name in self.musiclist:
             self.listbox.insert(tk.END, name)
+            previous_musiclist.append(name)
 
-        self.listbox.pack(fill=tk.BOTH, side=tk.LEFT, padx=10,pady=10)
         self.listbox.bind("<<ListboxSelect>>", self.handle_get_selection)
+        self.after(1000, self.update_file_list)
 
     def handle_get_selection(self, event):
         selected_indices = self.listbox.curselection()
